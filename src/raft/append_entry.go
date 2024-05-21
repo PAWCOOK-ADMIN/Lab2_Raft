8bc77f685b010c1dd5e307976a83705161f85507
pawcook <pawcook@163.com>
2024-05-21 22:40:29 +0800

    init

---
+++ src/raft/append_entry.go
@@ -0,0 +1,204 @@
+package raft
+
+type AppendEntriesArgs struct {
+	Term         int
+	LeaderId     int
+	PrevLogIndex int
+	PrevLogTerm  int
+	Entries      []Entry
+	LeaderCommit int
+}
+
+type AppendEntriesReply struct {
+	Term     int
+	Success  bool
+	Conflict bool
+	XTerm    int
+	XIndex   int
+	XLen     int
+}
+
+func (rf *Raft) appendEntries(heartbeat bool) {
+	lastLog := rf.log.lastLog()
+	for peer, _ := range rf.peers {
+		if peer == rf.me {
+			rf.resetElectionTimer()
+			continue
+		}
+		// rules for leader 3
+		if lastLog.Index >= rf.nextIndex[peer] || heartbeat {
+			nextIndex := rf.nextIndex[peer]
+			if nextIndex <= 0 {
+				nextIndex = 1
+			}
+			if lastLog.Index+1 < nextIndex {
+				nextIndex = lastLog.Index
+			}
+			prevLog := rf.log.at(nextIndex - 1)
+			args := AppendEntriesArgs{
+				Term:         rf.currentTerm,
+				LeaderId:     rf.me,
+				PrevLogIndex: prevLog.Index,
+				PrevLogTerm:  prevLog.Term,
+				Entries:      make([]Entry, lastLog.Index-nextIndex+1),
+				LeaderCommit: rf.commitIndex,
+			}
+			copy(args.Entries, rf.log.slice(nextIndex))
+			go rf.leaderSendEntries(peer, &args)
+		}
+	}
+}
+
+func (rf *Raft) leaderSendEntries(serverId int, args *AppendEntriesArgs) {
+	var reply AppendEntriesReply
+	ok := rf.sendAppendEntries(serverId, args, &reply)
+	if !ok {
+		return
+	}
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	if reply.Term > rf.currentTerm {
+		rf.setNewTerm(reply.Term)
+		return
+	}
+	if args.Term == rf.currentTerm {
+		// rules for leader 3.1
+		if reply.Success {
+			match := args.PrevLogIndex + len(args.Entries)
+			next := match + 1
+			rf.nextIndex[serverId] = max(rf.nextIndex[serverId], next)
+			rf.matchIndex[serverId] = max(rf.matchIndex[serverId], match)
+			DPrintf("[%v]: %v append success next %v match %v", rf.me, serverId, rf.nextIndex[serverId], rf.matchIndex[serverId])
+		} else if reply.Conflict {
+			DPrintf("[%v]: Conflict from %v %#v", rf.me, serverId, reply)
+			if reply.XTerm == -1 {
+				rf.nextIndex[serverId] = reply.XLen
+			} else {
+				lastLogInXTerm := rf.findLastLogInTerm(reply.XTerm)
+				DPrintf("[%v]: lastLogInXTerm %v", rf.me, lastLogInXTerm)
+				if lastLogInXTerm > 0 {
+					rf.nextIndex[serverId] = lastLogInXTerm
+				} else {
+					rf.nextIndex[serverId] = reply.XIndex
+				}
+			}
+
+			DPrintf("[%v]: leader nextIndex[%v] %v", rf.me, serverId, rf.nextIndex[serverId])
+		} else if rf.nextIndex[serverId] > 1 {
+			rf.nextIndex[serverId]--
+		}
+		rf.leaderCommitRule()
+	}
+}
+
+func (rf *Raft) findLastLogInTerm(x int) int {
+	for i := rf.log.lastLog().Index; i > 0; i-- {
+		term := rf.log.at(i).Term
+		if term == x {
+			return i
+		} else if term < x {
+			break
+		}
+	}
+	return -1
+}
+
+func (rf *Raft) leaderCommitRule() {
+	// leader rule 4
+	if rf.state != Leader {
+		return
+	}
+
+	for n := rf.commitIndex + 1; n <= rf.log.lastLog().Index; n++ {
+		if rf.log.at(n).Term != rf.currentTerm {
+			continue
+		}
+		counter := 1
+		for serverId := 0; serverId < len(rf.peers); serverId++ {
+			if serverId != rf.me && rf.matchIndex[serverId] >= n {
+				counter++
+			}
+			if counter > len(rf.peers)/2 {
+				rf.commitIndex = n
+				DPrintf("[%v] leader尝试提交 index %v", rf.me, rf.commitIndex)
+				rf.apply()
+				break
+			}
+		}
+	}
+}
+
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	DPrintf("[%d]: (term %d) follower 收到 [%v] AppendEntries %v, prevIndex %v, prevTerm %v", rf.me, rf.currentTerm, args.LeaderId, args.Entries, args.PrevLogIndex, args.PrevLogTerm)
+	// rules for servers
+	// all servers 2
+	reply.Success = false
+	reply.Term = rf.currentTerm
+	if args.Term > rf.currentTerm {
+		rf.setNewTerm(args.Term)
+		return
+	}
+
+	// append entries rpc 1
+	if args.Term < rf.currentTerm {
+		return
+	}
+	rf.resetElectionTimer()
+
+	// candidate rule 3
+	if rf.state == Candidate {
+		rf.state = Follower
+	}
+	// append entries rpc 2
+	if rf.log.lastLog().Index < args.PrevLogIndex {
+		reply.Conflict = true
+		reply.XTerm = -1
+		reply.XIndex = -1
+		reply.XLen = rf.log.len()
+		DPrintf("[%v]: Conflict XTerm %v, XIndex %v, XLen %v", rf.me, reply.XTerm, reply.XIndex, reply.XLen)
+		return
+	}
+	if rf.log.at(args.PrevLogIndex).Term != args.PrevLogTerm {
+		reply.Conflict = true
+		xTerm := rf.log.at(args.PrevLogIndex).Term
+		for xIndex := args.PrevLogIndex; xIndex > 0; xIndex-- {
+			if rf.log.at(xIndex-1).Term != xTerm {
+				reply.XIndex = xIndex
+				break
+			}
+		}
+		reply.XTerm = xTerm
+		reply.XLen = rf.log.len()
+		DPrintf("[%v]: Conflict XTerm %v, XIndex %v, XLen %v", rf.me, reply.XTerm, reply.XIndex, reply.XLen)
+		return
+	}
+
+	for idx, entry := range args.Entries {
+		// append entries rpc 3
+		if entry.Index <= rf.log.lastLog().Index && rf.log.at(entry.Index).Term != entry.Term {
+			rf.log.truncate(entry.Index)
+			rf.persist()
+		}
+		// append entries rpc 4
+		if entry.Index > rf.log.lastLog().Index {
+			rf.log.append(args.Entries[idx:]...)
+			DPrintf("[%d]: follower append [%v]", rf.me, args.Entries[idx:])
+			rf.persist()
+			break
+		}
+	}
+
+	// append entries rpc 5
+	if args.LeaderCommit > rf.commitIndex {
+		rf.commitIndex = min(args.LeaderCommit, rf.log.lastLog().Index)
+		rf.apply()
+	}
+	reply.Success = true
+}
+
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
+	return ok
+}
---
+++ src/raft/leader_election.go
@@ -0,0 +1,58 @@
+package raft
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// return currentTerm and whether this server
+// believes it is the leader.
+func (rf *Raft) GetState() (int, bool) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	term := rf.currentTerm
+	isleader := rf.state == Leader
+	return term, isleader
+}
+
+func (rf *Raft) resetElectionTimer() {
+	t := time.Now()
+	electionTimeout := time.Duration(150 + rand.Intn(150)) * time.Millisecond
+	rf.electionTime = t.Add(electionTimeout)
+}
+
+func (rf *Raft) setNewTerm(term int) {
+	if term > rf.currentTerm || rf.currentTerm == 0 {
+		rf.state = Follower
+		rf.currentTerm = term
+		rf.votedFor = -1
+		DPrintf("[%d]: set term %v\n", rf.me, rf.currentTerm)
+		rf.persist()
+	}
+}
+
+func (rf *Raft) leaderElection() {
+	rf.currentTerm++
+	rf.state = Candidate
+	rf.votedFor = rf.me
+	rf.persist()
+	rf.resetElectionTimer()
+	term := rf.currentTerm
+	voteCounter := 1
+	lastLog := rf.log.lastLog()
+	DPrintf("[%v]: start leader election, term %d\n", rf.me, rf.currentTerm)
+	args := RequestVoteArgs{
+		Term:         term,
+		CandidateId:  rf.me,
+		LastLogIndex: lastLog.Index,
+		LastLogTerm:  lastLog.Term,
+	}
+
+	var becomeLeader sync.Once
+	for serverId, _ := range rf.peers {
+		if serverId != rf.me {
+			go rf.candidateRequestVote(serverId, &args, &voteCounter, &becomeLeader)
+		}
+	}
+}
